c635461bb0db7aaee35cb6010d57456e27fd2f55
James Leslie <james@jamesleslie.net>
2018-03-24 00:53:18 +1100

    Add a tooltip to show extra info for fields

--- renderer/src/components/output/Label.tsx
+++ renderer/src/components/output/Label.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import * as ReactTooltip from 'react-tooltip'
 
 interface LabelProps {
     text: string,
@@ -8,5 +9,6 @@ interface LabelProps {
 
 export const Label = (props: LabelProps) => (
     <label htmlFor={props.htmlFor} className="mr-2 h6">{props.text + ' '}
-        {props.info && <img className="info-icon" src="/icons/info-circle.svg"/>}
+        {props.info && <img className="info-icon" data-tip={props.info} src="/icons/info-circle.svg"/>}
+        <ReactTooltip className="tool-tip" place="right" effect="solid"/>
     </label>)
