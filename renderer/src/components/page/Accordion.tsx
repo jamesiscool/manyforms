3fb4720a95443fc40e5d3a5c9f35de33d4204a6f
James Leslie <james@jamesleslie.net>
2018-04-19 23:44:52 +1000

    Remove react-scroll and do it manually

--- renderer/src/components/page/Accordion.tsx
+++ renderer/src/components/page/Accordion.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Element, scroller } from 'react-scroll'
+import { findDOMNode } from 'react-dom'
 import { createKey } from '../../util'
 import { FormElementProps } from '../FormElement'
 import { Page } from './Page'
@@ -15,6 +15,7 @@ interface AccordionState {
 }
 
 export class Accordion extends React.Component<AccordionProps, AccordionState> {
+    private currentPageRef: Page | null
 
     constructor(props: AccordionProps) {
         super(props)
@@ -26,7 +27,11 @@ export class Accordion extends React.Component<AccordionProps, AccordionState> {
     }
 
     componentDidUpdate() {
-        scroller.scrollTo('page_' + this.state.currentPage, {offset: -100})
+        if (this.currentPageRef) {
+            const domNode = findDOMNode(this.currentPageRef) as Element
+            const domRect = domNode.getBoundingClientRect()
+            window.scroll({top: domRect.top - 100})
+        }
     }
 
     setCurrentPage(index: number) {
@@ -59,11 +64,10 @@ export class Accordion extends React.Component<AccordionProps, AccordionState> {
                 {this.props.definition.children!.map((page, index) => {
                     return <div className="card" key={createKey()}>
                         <div className="card-header cursor-pointer" onClick={() => this.goToPage(index)}>
-                            <Element name={'page_' + index}>
-                                <h3 className="d-inline">{page.attributes.label}</h3>{index < this.state.currentPage && <a className="d-inline text-muted"> <u>edit</u></a>}
-                            </Element>
+                            <h3 className="d-inline">{page.attributes.label}</h3>{index < this.state.currentPage && <a className="d-inline text-muted"> <u>edit</u></a>}
                         </div>
                         {this.state.currentPage === index && <Page
+                            ref={(ref) => this.currentPageRef = ref}
                             definition={page}
                             goToPreviousPage={this.goToPreviousPage}
                             goToNextPage={this.goToNextPage}
