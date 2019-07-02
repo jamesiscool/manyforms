3562aaa7ffa81668225935423d3774413e68afdc
James Leslie <james@jamesleslie.net>
2019-07-02 11:27:08 +1000

    Add axios for http fetching

--- renderer/src/index.tsx
+++ renderer/src/index.tsx
@@ -1,19 +1,18 @@
+import axios from 'axios'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Form} from './Form'
 import FormDef from './FormDef'
 import './index.css'
 
-fetch('exampleFormDefinition.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(response.statusText)
-        }
-        return response.json()
-    })
-    .then((formDef: FormDef) => {
+axios.get<FormDef>('exampleFormDefinition.json')
+    .then(function (response) {
         ReactDOM.render(
-            <Form formDef={formDef}/>,
+            <Form formDef={response.data}/>,
             document.getElementById('form')
         )
+    })
+    .catch(function (error) {
+        console.error(error)
+        throw new Error(error)
     })
